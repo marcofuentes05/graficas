963260bf4665f0a851011cb115c4534601a6d8cc
Marco <marcofuentes1607@gmail.com>
2020-07-26 03:11:01 -0600

    Update README

--- SR2.cpp
+++ SR2.cpp
@@ -363,9 +363,6 @@ int main(){
     r.glDrawSquare(ld ,lu ,rd, ru);
   }
 
-  int lista[][2] = { {165, 380} , {185, 360} , {180, 330} , {207, 345} , {233, 330} , {230, 360} , {250, 380} , {220, 385} , {205, 410} , {193, 383} };
-  r.drawPolygon(lista , 10 ); 
-
   //Guardo el archivo
   r.glFinish();
 }
